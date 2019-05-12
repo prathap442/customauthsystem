dc5ec5a2a5f6aee9a8b5c927ef37f44fd892e7e9
prathap <prathap.mohan@kloctechnologies.co.in>
2019-05-12 22:34:26 +0530

    got error and need to fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
-# the users controller here 
+# the users controller here
+# got an error
+=begin 
+ NameError:
+   uninitialized constant UsersController::User
+   This is due to the User model being missed  
+=end
 class UsersController < ApplicationController
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if(@user.save)
+      render json: {
+        msg: 'User got successfully registered but not verified', 
+        status: 201
+      }
+    else
+      render json: {
+        msg: 'User not got created',
+        status: 200
+      }
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name,:mobile,:password)
+  end
 end
